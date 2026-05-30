6807ec14c10ea478b676cc63424c3119c03ffee3
OCruzGarcia <cruzgarciaorlando@outlook.com>
2026-05-30 16:27:22 -0600

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -148,7 +148,7 @@ Quiero información para un pastel personalizado 🎂
 📝 Descripción:
 ${descripcion}`;
 
-  const phone = "50583750020";
+  const phone = "50586442219";
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(mensaje)}`;
 
   // Redirección y limpieza
